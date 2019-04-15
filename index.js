9f1bb4d8cc0dc43b839e98baedc0c5fa32b1150c
Rutger McKenna <rutgerjmckenna@gmail.com>
2019-04-15 19:45:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function 
+function logShout(string) {
+  console.log(shout(string))
+}
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+function sayHiToGrandma(string) {
+  console.log(whisper.string)
+  return "I can't hear you!"
+}
+
+if (string === string.toUpperCase()) {
+    console.log ("YES INDEED!");
+}
+else (string)
